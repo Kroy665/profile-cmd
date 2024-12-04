569818855acf19c62c37eff31347dcdb5231a0a5
Koushik Roy <koushikroy@Koushiks-MacBook-Air.local>
2024-12-04 18:27:27 +0530

    deploy commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://kroy665.github.io/profile-cmd' : './',
 };
 
 export default nextConfig;
